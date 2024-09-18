858e5517736d51373fa088e0c739b0b9e2314616
vadim <glushko311@gmail.com>
2024-09-18 12:07:39 +0000

    try 1

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ handler = Mangum(app)
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to your personal World !!!!"}
+    return {"message": "Welcome to your personal World !!!!!"}
 
 
 @app.get("/random-book")
